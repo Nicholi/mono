742bddbd55a597e7ca103b0df77d88952c8c583e
Nicholi <nschell@gmail.com>
2014-07-21 04:06:44 -0400

    Revert "ClaimsApi"
    This reverts commit 2fa6530559ea293700566a2eab8a6d7c85639268, reversing
    changes made to d76fca6e59061297fd8ec16f39447be6f709a34d.

--- mcs/class/corlib/System.Security.Principal/WindowsIdentity.cs
+++ mcs/class/corlib/System.Security.Principal/WindowsIdentity.cs
@@ -33,9 +33,6 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
 using System.Security.Permissions;
-#if NET_4_5
-using System.Security.Claims;
-#endif
 
 namespace System.Security.Principal {
 
@@ -75,9 +72,6 @@ namespace System.Security.Principal {
 
 		[SecurityPermission (SecurityAction.Demand, ControlPrincipal=true)]
 		public WindowsIdentity (IntPtr userToken, string type, WindowsAccountType acctType, bool isAuthenticated)
-#if NET_4_5
-			: base(null, null, type, null, null)
-#endif
 		{
 			_type = type;
 			_account = acctType;
@@ -117,16 +111,6 @@ namespace System.Security.Principal {
 		{
 			_info = info;
 		}
-#if NET_4_5
-		[SecurityCritical]
-		internal WindowsIdentity(ClaimsIdentity claimsIdentity, IntPtr userToken)
-			: base((IIdentity)claimsIdentity)
-		{
-			if (!(userToken != IntPtr.Zero) || userToken.ToInt64() <= 0L)
-				return;
-			this.SetToken(userToken);
-		}
-#endif
 
 		[ComVisible (false)]
 		public void Dispose ()
@@ -192,7 +176,6 @@ namespace System.Security.Principal {
 		sealed override
 #endif
 		public string AuthenticationType {
-#endif
 			get { return _type; }
 		}
 
@@ -202,18 +185,11 @@ namespace System.Security.Principal {
 		}
 
 #if NET_4_5
-<<<<<<< HEAD
 		override
 #else
 		virtual
 #endif
 		public bool IsAuthenticated
-=======
-		public override bool IsAuthenticated
-#else
-		public virtual bool IsAuthenticated
-#endif
->>>>>>> matthid/claimsApi
 		{
 			get { return _authenticated; }
 		}
@@ -243,12 +219,6 @@ namespace System.Security.Principal {
 				return _name; 
 			}
 		}
-#if NET_4_5
-		internal ClaimsIdentity CloneClaimsIdentity()
-		{
-			return base.Clone();
-		}
-#endif
 
 		public virtual IntPtr Token
 		{
